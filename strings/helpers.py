1b11bbf7ca1bdcd9d6ac0ebf32665306cb12f83b
yildizcavres <129795323+yildizcavres@users.noreply.github.com>
2023-04-06 16:13:02 +0300

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -7,116 +7,74 @@
 #
 # All rights reserved.
 
-HELP_1 = """✅**<u>𝖡𝖺𝗌𝗂𝗍 𝖪𝗈𝗆𝗎𝗍𝗅𝖺𝗋:</u>**
-
+HELP_1 = """✅ <u>𝖬𝗎̈𝗓𝗂𝗄 𝖪𝗈𝗆𝗎𝗍𝗅𝖺𝗋 :</u>
 ➡️ sahip.. @cavres34 .. https://t.me/okyanussohbett
-
-» /bul > 𝖲𝖺𝗋𝗄𝗂 𝗂𝗇𝖽𝗂𝗋 .
-
-» /oynat > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗈𝗒𝗇𝖺𝗍 .
-
-» /durdur > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝖽𝗎𝗋𝖽𝗎𝗋 .
-
-» /devam > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗌𝗎𝗋𝖽𝗎𝗋 .
-
-» /atla > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝖺𝗍𝗅𝖺 .
-
-» /son > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗌𝗈𝗇𝗅𝖺𝗇𝖽𝗂𝗋 .
-
-» /karistir > 𝖲𝖺𝗋𝗄𝗂 𝗅𝗂𝗌𝗍𝖾𝗌𝗂𝗇𝗂 𝗄𝖺𝗋𝗂𝗌𝗍𝗂𝗋 .
-
-» /tekrarla > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗍𝖾𝗄𝗋𝖺𝗋𝗅𝖺 .
-
-» /ileri > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗂𝗅𝖾𝗋𝗂 𝗌𝖺𝗋 .
-
 » /vplay > 𝖵𝗂𝖽𝖾𝗈 𝗈𝗒𝗇𝖺𝗍 .
-
+» /oynat > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗈𝗒𝗇𝖺𝗍 .
+» /durdur > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝖽𝗎𝗋𝖽𝗎𝗋 .
+» /devam > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗌𝗎𝗋𝖽𝗎𝗋 .
+» /atla > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝖺𝗍𝗅𝖺 .
+» /son > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗌𝗈𝗇𝗅𝖺𝗇𝖽𝗂𝗋 .
+» /karistir > 𝖲𝖺𝗋𝗄𝗂 𝗅𝗂𝗌𝗍𝖾𝗌𝗂𝗇𝗂 𝗄𝖺𝗋𝗂𝗌𝗍𝗂𝗋 .
+» /tekrarla > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗍𝖾𝗄𝗋𝖺𝗋𝗅𝖺 .
+» /ileri > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗂𝗅𝖾𝗋𝗂 𝗌𝖺𝗋 .
+» /seekback > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗀𝖾𝗋𝗂𝗒𝖾 𝗌𝖺𝗋 .
+» /playforce > 𝖲ı𝗋𝖺𝗒ı 𝖻𝗈𝗓𝗆𝖺𝖽𝖺𝗇 𝗂𝗌𝗍𝖾𝖽𝗂𝗀̆𝗂𝗇𝗂𝗓 𝖲̧𝖺𝗋𝗄ı𝗒ı 𝗈𝗒𝗇𝖺𝗍ı𝗋 .
+» /vplayforce > 𝖲ı𝗋𝖺𝗒ı 𝖻𝗈𝗓𝗆𝖺𝖽𝖺𝗇 𝗂𝗌𝗍𝖾𝖽𝗂𝗀̆𝗂𝗇𝗂𝗓 𝖵𝗂𝖽𝖾𝗈𝗒𝗎 𝗈𝗒𝗇𝖺𝗍ı𝗋 .
 » /playlist > 𝖯𝗅𝖺𝗒𝗅𝗂𝗌𝗍𝗂𝗇𝗂𝗓𝗂 𝖮𝗒𝗇𝖺𝗍𝗂𝗋 .
-
+» /deleteplaylist > 𝖯𝗅𝖺𝗒𝗅𝗂𝗌𝗍𝗂𝗇𝗂𝗓𝗂 𝖲𝗂𝗅𝖾𝗋 .
+» /restart > 𝖡𝗈𝗍𝗎 𝖸𝖾𝗇𝗂𝖽𝖾𝗇 𝖡𝖺𝗌̧𝗅𝖺𝗍ı𝗋.
 » /reload > 𝖠𝖽𝗆𝗂𝗇 𝖫𝗂𝗌𝗍𝖾𝗌𝗂𝗇𝗂 𝖦𝗎𝗇𝖼𝖾𝗅𝗅𝖾 .
-
+» /auth > 𝖪𝗎𝗅𝗅𝖺𝗇ı𝖼ı𝗒𝖺 𝖸𝖾𝗍𝗄𝗂 𝖵𝖾𝗋𝗂𝗋 .
+» /unauth > 𝖪𝗎𝗅𝗅𝖺𝗇ı𝖼ı 𝖸𝖾𝗍𝗄𝗂𝗌𝗂𝗇𝗂 𝖠𝗅ı𝗋 .
+» /lang > 𝖡𝗈𝗍 𝗂𝖼̧𝗂𝗇 𝖻𝗂𝗋 𝖽𝗂𝗅 𝖻𝖾𝗅𝗂𝗋𝗅𝖾𝗒𝗂𝗇 .
 """
 
-
-HELP_2 = """✅<u>**𝖦𝗋𝗈𝗎𝗉 :**</u>
-
-• 𝖫𝖾𝗍'𝗌 𝖦𝗈 => @okyanussohbett
-
+HELP_2 = """✅ <u>𝖪𝖺𝗇𝖺𝗅 𝖮𝗒𝗇𝖺𝗍𝗆𝖺 :</u>
+◽ 𝖡𝗈𝗍𝗎 𝗄𝖺𝗇𝖺𝗅𝖺 𝖾𝗄𝗅𝖾𝗒𝗂𝗉 𝖸𝗈̈𝗇𝖾𝗍𝗂𝖼𝗂 𝗒𝖺𝗉ı𝗇, 𝖪𝖺𝗇𝖺𝗅 𝗂𝖽'𝗌𝗂𝗇𝗂 𝖺𝗅ı𝗉 𝗄𝖺𝗇𝖺𝗅ı𝗇 𝖻𝖺𝗀̆𝗅ı 𝗈𝗅𝖽𝗎𝗀̆𝗎 𝗀𝗋𝗎𝖻𝖺 𝗀𝗂𝗋𝗂𝗉 /channelplay ( 𝖪𝖺𝗇𝖺𝗅 𝗂𝖽 ) 𝗒𝖺𝗓ı𝗇 . 𝖳𝖺𝖻𝗂 𝖻𝗎𝗇𝗅𝖺𝗋ı 𝗄𝖺𝗇𝖺𝗅 𝗌𝖺𝗁𝗂𝖻𝗂 𝗒𝖺𝗉𝗆𝖺𝗅ı , 𝗄𝗈𝗆𝗎𝗍𝗅𝖺𝗋ı 𝖽𝖺 𝗌𝖾𝗌 𝗒𝖾𝗍𝗄𝗂𝗌𝗂 𝗈𝗅𝖺𝗇 𝗄𝗂𝗌̧𝗂𝗅𝖾𝗋 𝗄𝗎𝗅𝗅𝖺𝗇ı𝗋 .
+» /cplay > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗈𝗒𝗇𝖺𝗍 .
+» /cpause > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝖽𝗎𝗋𝖽𝗎𝗋 .
+» /cresume > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗌𝗎𝗋𝖽𝗎𝗋 .
+» /cskip > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝖺𝗍𝗅𝖺 .
+» /cend > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗌𝗈𝗇𝗅𝖺𝗇𝖽𝗂𝗋 .
+» /cshuffle > 𝖲𝖺𝗋𝗄𝗂 𝗅𝗂𝗌𝗍𝖾𝗌𝗂𝗇𝗂 𝗄𝖺𝗋𝗂𝗌𝗍𝗂𝗋 .
+» /cloop > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗍𝖾𝗄𝗋𝖺𝗋𝗅𝖺 .
+» /cseek > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗂𝗅𝖾𝗋𝗂 𝗌𝖺𝗋 .
+» /cseekback > 𝖲𝖺𝗋𝗄𝗂𝗒𝗂 𝗀𝖾𝗋𝗂𝗒𝖾 𝗌𝖺𝗋 .
+» /cvplay > 𝖵𝗂𝖽𝖾𝗈 𝗈𝗒𝗇𝖺𝗍 .
+» /cplayforce > 𝖲ı𝗋𝖺𝗒ı 𝖻𝗈𝗓𝗆𝖺𝖽𝖺𝗇 𝗂𝗌𝗍𝖾𝖽𝗂𝗀̆𝗂𝗇𝗂𝗓 𝖲̧𝖺𝗋𝗄ı𝗒ı 𝗈𝗒𝗇𝖺𝗍ı𝗋 .
+» /cvplayforce > 𝖲ı𝗋𝖺𝗒ı 𝖻𝗈𝗓𝗆𝖺𝖽𝖺𝗇 𝗂𝗌𝗍𝖾𝖽𝗂𝗀̆𝗂𝗇𝗂𝗓 𝖵𝗂𝖽𝖾𝗈𝗒𝗎 𝗈𝗒𝗇𝖺𝗍ı𝗋 .
 """
 
 
 HELP_3 = """✅<u>**𝖪𝖺𝗇𝖺𝗅 :**</u>
-
-• 𝖫𝖾𝗍'𝗌 𝖦𝗈 => @okyanussohbett
-
+• 𝖫𝖾𝗍'𝗌 𝖦𝗈 => [)
+ 
 """
 
 
-HELP_4 = """✅<u>**𝖮𝗐𝗇𝖾𝗋 :**</u>
-
-• 𝖫𝖾𝗍'𝗌 𝖦𝗈 => @cavres34
-
+HELP_4 = """✅<u>**𝖲𝖺𝗁𝗂𝗉 :**</u>
+• 𝖫𝖾𝗍'𝗌 𝖦𝗈 => [𝖲 𝖠 𝖧 𝖨 𝖯](@cavres34)
+ 
 """
-HELP_5 = """🔰**<u>𝖲𝗎𝖽𝗈 𝖪𝗈𝗆𝗎𝗍𝗅𝖺𝗋 :</u>**
-/addsudo [Username o𝗋 Reply to a user]
-/delsudo [Username or Reply to a user]
-
-🛃**<u>HEROKU:</u>**
-/usage - Dyno Usage.
-
-🌐**<u>CONFIG VARS:</u>**
-/get_var - Get a config var from Heroku or .env.
-/del_var - Delete any var on Heroku or .env.
-/set_var [Var Name] [Value] - Set a Var or Update a Var on heroku or .env. Seperate Var and its Value with a space.
-
-🤖**<u>BOT COMMANDS:</u>**
-/reboot - Reboot your Bot. 
-/update - Update Bot.
-/speedtest - Check server speeds
-/maintenance [enable / disable] 
-/logger [enable / disable] - Bot logs the searched queries in logger group.
-/get_log [Number of Lines] - Get log of your bot from heroku or vps. Works for both.
-/autoend [enable|disable] - Enable Auto stream end after 3 mins if no one is listening.
-
-📈**<u>STATS COMMANDS:</u>**
-/activevoice - Check active voice chats on bot.
-/activevideo - Check active video calls on bot.
-/stats - Check Bots Stats
-
-⚠️**<u>BLACKLIST CHAT FUNCTION:</u>**
-/blacklistchat [CHAT_ID] - Blacklist any chat from using Music Bot
-/whitelistchat [CHAT_ID] - Whitelist any blacklisted chat from using Music Bot
-/blacklistedchat - Check all blacklisted chats.
-
-👤**<u>BLOCKED FUNCTION:</u>**
-/block [Username or Reply to a user] - Prevents a user from using bot commands.
-/unblock [Username or Reply to a user] - Remove a user from Bot's Blocked List.
-/blockedusers - Check blocked Users Lists
-
-👤**<u>GBAN FUNCTION:</u>**
-/gban [Username or Reply to a user] - Gban a user from bot's served chat and stop him from using your bot.
-/ungban [Username or Reply to a user] - Remove a user from Bot's gbanned List and allow him for using your bot
-/gbannedusers - Check Gbanned Users Lists
-
-🎥**<u>VIDEOCALLS FUNCTION:</u>**
-/set_video_limit [Number of Chats] - Set a maximum Number of Chats allowed for Video Calls at a time. Default to 3 chats.
-/videomode [download|m3u8] - If download mode is enabled, Bot will download videos instead of playing them in M3u8 form. ByDefault to M3u8. You can use download mode when any query doesnt plays in m3u8 mode.
-
-⚡️**<u>PRIVATE BOT FUNCTION:</u>**
-/authorize [CHAT_ID] - Allow a chat for using your bot.
-/unauthorize [CHAT_ID] - Disallow a chat from using your bot.
-/authorized - Check all allowed chats of your bot.
-
-🌐**<u>BROADCAST FUNCTION:</u>**
-/broadcast [Message or Reply to a Message] - Broadcast any message to Bot's Served Chats.
-
-<u>options for broadcast:</u>
-**-pin** : This will pin your message 
-**-pinloud** : This will pin your message with loud notification
-**-user** : This will broadcast your message to the users who have started your bot.
-**-assistant** : This will broadcast your message from assistant account of your bot.
-**-nobot** : This will force your bot to not broadcast message
-
-**Example:** `/broadcast -user -assistant -pin Hello Testing`
-
-"""
+HELP_5 = """** 🔰<u>Sudo Komutlar :</u>
+» /addsudo [ Kullanıcı Kimliği ]
+» /delsudo [ Kullanıcı Kimliği ]
+» /sudolist [ listeyi görüntüle ]
+» /reboot  -> [ botu yeniden başlatır ]
+» /logger  -> [ log grubu içindir ]
+» /reklam  -> [ reklam verir ]
+» /vars  -> [ bottan detayli bilgi verir ]
+» /limit -> [ video limiti ayarlar ]
+» /blacklistchat  -> [ grubu yasaklar ]
+» /whitelistchat -> [ grub yasağı kaldırır ]
+» /blacklistedchat [ yasaklı listeyi gösterir ]
+» /activevoice  -> [ aktif sesli gruplar ]
+» /activevideo  -> [ aktif videolu gruplar ]
+» /block  -> [ kullanıcıyı Yasaklar ]
+» /unblock  -> [ yasağı Kaldırır]
+» /blocklist  -> [ yasaklı listeyi gösterir ]
+» /gban  -> [ kullanıcıyı grublardan banlar ]
+» /ungban  -> [ kullanıcı banını kaldırır ]
+» /gbanlist  -> [ banlananlar listesini gösterir ]
+**"""
